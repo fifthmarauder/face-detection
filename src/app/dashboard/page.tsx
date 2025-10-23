f4099d9eecedcb2c50a0baad680529ae0c7b28e0
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-10-23 17:31:19 +0500

    Added main card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import styles from './page.module.css'
 
 
 const Page = () => {
   return (
-    <div >
-      
+    <>
+    <div className={styles.container}>
+        <div className={styles.card}>
+            <div className={styles.cardTitle}>
+                Upload Image
+            </div>
+            <div className={styles.urlContainer}>
+                <div className={styles.urlTitle}>Enter Image URL</div>
+                <div className={styles.urlLinkContainer}>
+                    <div className={styles.urlLink}>
+                        url box
+                    </div>
+                    <div><button>Submit</button></div>
+                </div>
+            </div>
+            <div className={styles.divider}>
+                <div className={styles.line}></div>
+                <div>OR</div>
+                <div className={styles.line}></div>
+            </div>  
+            <div className={styles.imageContainer}>
+                <div className={styles.urlTitle}>
+                    Upload from Computer
+                </div>
+                <div>
+                    <button className={styles.button}>
+                        Choose Image File
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
+    </>
   )
 }
 
